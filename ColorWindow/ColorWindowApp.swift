d57387b2545ace32d27c01dfe4dd6c677dbf95d0
Мария Шабанова <mariasha312@gmail.com>
2024-04-21 20:08:39 +0300

    Initial Commit

---
+++ ColorWindow/ColorWindowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorWindowApp.swift
+//  ColorWindow
+//
+//  Created by Мария Шабанова on 21.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct ColorWindowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
